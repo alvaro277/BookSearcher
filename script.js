0201de9c054c2da3a3708e3376727e3b43e9e7b0
alvaro277 <alvaropineda89can@gmail.com>
2025-05-01 18:07:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Click handler for search button
+let input = document.getElementById("search-bar");
+
+const captureSearchValue = () => {
+  let value = input.value;
+  return value;
+};
+
+// Filter books based on search input
+const filterBooks = (string,array) => {
+  let flattenedBooks = flattenObjectValuesIntoArray(books);
+  let filteredBooks = flattenedBooks.filter( book => book.includes(string))
+  return filteredBooks;
+};
+
+// Empty the book list container, iterate over list of filtered books, return list of books formatted as HTML using the function in `helper.js` 
+const structureBooksAsHtml = (listOfBooks) => {
+  let HTMLBooks = listOfBooks.map( book => structureBookAsHtml(book));
+  return HTMLBooks;
+};
+
+// Handler triggered when a user clickers the "Search" button. Chains previously defined functions together to filter books based on the search value, formats the books as HTML and renders them to the DOM
+const searchBtnClickHandler = (books) => {
+  let value = captureSearchValue();
+  let filteredBooks = filterBooks(value,books);
+  structureBooksAsHtml(filteredBooks);
+}
+
+// Grab search button from the DOM
+let searchBtn = document.getElementById("search-btn");
+
+// Attach an event listener to the search button
+searchBtn.addEventListener("click", () => { searchBtnClickHandler(books) });
