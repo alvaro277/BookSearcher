69e3553f409bc9d7825be65e802e2f3c2272004c
alvaro277 <alvaropineda89can@gmail.com>
2025-05-01 20:51:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,16 +40,15 @@ const filterBooks = (string,array) => {
 
 // Empty the book list container, iterate over list of filtered books, return list of books formatted as HTML using the function in `helper.js` 
 const structureBooksAsHtml = (listOfBooks) => {
-  let HTMLBooks = listOfBooks.map( book => structureBookAsHtml(book));
-  console.log(HTMLBooks);
-  return HTMLBooks;
+  return listOfBooks.map( book => structureBookAsHtml(book));  
 };
 
 // Handler triggered when a user clickers the "Search" button. Chains previously defined functions together to filter books based on the search value, formats the books as HTML and renders them to the DOM
 const searchBtnClickHandler = (books) => {
   let value = captureSearchValue();
   let filteredBooks = filterBooks(value,books);
-  structureBooksAsHtml(filteredBooks);
+  let arrayHTML =structureBooksAsHtml(filteredBooks);
+  renderBooksToDom(arrayHTML);
 }
 
 // Grab search button from the DOM
